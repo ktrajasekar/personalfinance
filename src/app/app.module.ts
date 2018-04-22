e33444f5514d40d058da6dbcf4f6d17a208320b1
Rajasekar T <kt.rajasekar@gmail.com>
2018-04-22 16:54:23 +0530

    Routing Introduced - NgModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes  } from '@angular/router';
+import { ROUTING } from './app.routing';
 
-
+// Components
 import { AppComponent } from './app.component';
+import { CalcComponent } from './calc/calc.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcComponent,
+    HomeComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ROUTING,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.css']
+})
+export class CalcComponent implements OnInit {
+
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+    console.log("ini");
+  }
+
+}
